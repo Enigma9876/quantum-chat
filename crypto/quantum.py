786b79d2ff36f1fd3e27aedf1787df9e0878642d
swordmasterliam <98477358+swordmasterliam@users.noreply.github.com>
2026-02-11 20:12:56 +0000

    temp files

---
+++ crypto/quantum.py
@@ -0,0 +1,17 @@
+import sys, qiskit 
+
+class QuantumCipher:
+    id = "quantum"
+
+    def _process(self, text: str, key: int, decrypt: bool = False) -> str:
+        return text
+
+
+
+    def encrypt(self, plaintext: bytes, key: int, **kwargs) -> bytes:
+        return plaintext
+
+
+
+    def decrypt(self, ciphertext: bytes, key: int, meta: dict = None) -> bytes:
+        return ciphertext
