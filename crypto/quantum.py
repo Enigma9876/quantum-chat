89d36e46a588b7ddd632e3bc4ca1ac3dbdf6b7f5
swordmasterliam <98477358+swordmasterliam@users.noreply.github.com>
2026-03-10 20:35:49 +0000

    Quantum layout done

--- crypto/quantum.py
+++ crypto/quantum.py
@@ -9,7 +9,14 @@ class QuantumCipher:
 
 
     def __init__(self):
-        self.backend = BasicProvider().get_backend('bsk_simulator')
+        self.backend = None #BasicProvider().get_backend('bsk_simulator')
+        self.service = None # this will be used to store the quantum service (like IBMQ or something) if we want to use real quantum hardware
+        self.set_backend()
+
+
+
+    def set_backend(self):
+        pass
 
 
 
@@ -25,3 +32,34 @@ class QuantumCipher:
 
     def decrypt(self, ciphertext: bytes, key: int, meta: dict = None) -> bytes:
         return ciphertext
+    
+
+
+    def _one_time_pad(self, text: str, key_hex : str) -> str:
+        # this will use XOR to encrypt/decrypt
+
+        text_bytes = text.encode('utf-8')
+        text_int = int.from_bytes(text_bytes, 'big') #the reason this has the 'big' (note 'big' is big-endian and that is the standard for most systems) is because we want to convert the text into an integer representation that can be easily manipulated using bitwise operations
+
+
+        #we want to repeat the key if its to short (THIS WOULD NOT BE DONE IN REAL OTP)
+        key_int = int
+        if key_int == 0: key_int = 1 # to avoid division by zero
+
+        while key_int.bit_length() < text_int.bit_length():
+            key_int = (key_int << key_int.bit_length()) | key_int
+
+        #XOR that bish
+        cipher_int = text_int ^ key_int
+
+        return hex(cipher_int)[2:] # convert back to hex string, and removes da "0x" prefix
+    
+
+
+    def _simulate_bb84_key_exchange():
+        pass
+
+
+    def _run_circuit(self, circuit):
+        #this needs support for running on sim or real quantum hardware
+        pass
